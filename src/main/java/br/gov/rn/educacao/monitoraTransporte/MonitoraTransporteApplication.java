1a11a42113c3f4ffb614ddf8ab646d54ca394899
usuariodebruno <usuariodebruno@gmail.com>
2024-06-10 09:56:41 -0300

    projeto inicial

---
+++ src/main/java/br/gov/rn/educacao/monitoraTransporte/MonitoraTransporteApplication.java
@@ -0,0 +1,13 @@
+package br.gov.rn.educacao.monitoraTransporte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoraTransporteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitoraTransporteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/gov/rn/educacao/monitoraTransporte/MonitoraTransporteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.rn.educacao.monitoraTransporte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoraTransporteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
